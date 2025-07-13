26729a59c8872ae24488b5ece7b17f7df25aa5c3
MercyXp <munzenzimercy2024@gmail.com>
2025-07-13 09:38:43 +0200

    main.ts compiled

--- 0x04-TypeScript/task_1/webpack.config.js
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -4,16 +4,20 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
+  mode: "development", // <-- 🔧 Fix for the missing mode warning
   entry: "./js/main.ts",
   devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true
-        }
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true
+          }
+        },
+        exclude: /node_modules/
       }
     ]
   },
@@ -21,17 +25,24 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"]
   },
   devServer: {
-    contentBase: "./dist"
+    static: {
+      directory: path.join(__dirname, "dist") // <-- Updated from deprecated `contentBase`
+    },
+    compress: true,
+    port: 9000,
+    open: true
   },
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      template: "index.html", // Optional: If you have a template
       title: "Development"
     })
   ],
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    clean: true // optional, but automatically cleans output folder in webpack 5
   }
 };
