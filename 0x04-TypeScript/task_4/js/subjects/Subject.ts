308e3fcfafd608132fb9afd2bf4e7a866b98f62c
MercyXp <munzenzimercy2024@gmail.com>
2025-07-13 11:15:06 +0200

    subjects created

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+/// <reference path="./Teacher.ts" />
+
+namespace Subjects {
+    export class Subject {
+        teacher: Subjects.Teacher;
+
+        setTeacher(teacher: Subjects.Teacher): void {
+            this.teacher = teacher;
+        }
+    }
+}
